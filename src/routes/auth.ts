e10cc015e4abad97a37e3e27eb45a5f61fe31d01
Utkarsh Dhiman <47657465+utkarshdhiman48@users.noreply.github.com>
2024-09-03 23:42:50 +0530

    add: auth route (#19)

---
+++ src/routes/auth.ts
@@ -0,0 +1,40 @@
+import User from "@/models/user";
+import express from "express";
+import bcrypt from "bcrypt";
+import Joi from "joi";
+
+function validateUser(user: { email: string; password: string }) {
+  const schema = Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  });
+
+  return schema.validate(user);
+}
+
+const router = express.Router();
+
+router.post("/login", async (req, res) => {
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(404).send("User not found");
+
+  const passwordMatched = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+  if (!passwordMatched) return res.status(400).send("Invalid password");
+
+  const token = user.generateAuthToken();
+
+  return res.header("x-auth-token", token).json({
+    name: user.name,
+    email: user.email,
+    id: user._id,
+    phone: user.phone,
+  });
+});
+
+export default router;
