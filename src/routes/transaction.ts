157bf1e0bfb0b4b5208252cdc201c9489ce7f4b8
Utkarsh Dhiman <47657465+utkarshdhiman48@users.noreply.github.com>
2024-09-28 23:00:39 +0530

    20-update-transaction (#40)
    * add: route for updating transaction by id
    
    * fix: transaction update validation
    
    * fix: remove condition for not only created at and add update values

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import Transaction, {
   validate as validateTransaction,
+  validateUpdateTransaction,
 } from "@/models/transaction";
+import jwt from "jsonwebtoken";
+import { IUserTokenPaylaod } from "@/models/user";
 
 const router = express.Router();
 
@@ -22,6 +25,35 @@ router.post("/", async (req, res) => {
   await transaction.save();
 
   res.json(transaction);
+});
+
+router.patch("/:transactionId", async (req, res) => {
+  const { error } = validateUpdateTransaction(req.body);
+  if (error) return res.status(400).send(error.message);
+
+  const token = req.headers["x-auth-token"] as string;
+  const userId = (jwt.decode(token) as IUserTokenPaylaod).id;
+
+  const updatedTransaction = await Transaction.findOneAndUpdate(
+    {
+      _id: req.params.transactionId,
+      $or: [{ createdBy: userId }, { "distribution.person": userId }],
+    },
+    {
+      $set: {
+        ...req.body,
+        updatedBy: userId,
+        updatedAt: new Date(),
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!updatedTransaction) return res.status(404).send("Transaction not found");
+
+  res.json(updatedTransaction);
 });
 
 export default router;
